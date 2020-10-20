fd66aac2a7e7d2c0d28a17282713f7e3ca8e9dea
Men-HuLu <15705850186@163.com>
2020-10-20 23:11:43 +0800

    基地址修改

--- src/config/api.js
+++ src/config/api.js
@@ -1,21 +1,19 @@
 import wxHelper from '../utils/wxHelper';
 
 const env = 'Debug';
-const urlLogin = env === 'Debug' ? 'http://192.168.1.97:5500/login/wxcustom' : 'http://192.168.1.97:5500/login/wxcustom';
+const base = env === 'Debug' ? 'http://192.168.1.97:5500/login/wxcustom' : 'http://192.168.1.97:5500/login/wxcustom';
 
 const urlConfig = {
-  host: '',
-  mine: '/mine',
+  host: base,
+  login: '/login',
   index: '/index',
-  promote: '/promote',
-  coupon: '/coupon',
-  product: '/product',
-  cart: '/cart',
-  business: '/business',
-  phone: '/mine/phone',
-  order: '/order',
-  policy: '/policy',
-  pay: '/pay'
+  search: '/search',
+  advice: '/advice',
+  bookConnent: '/bookConnent',
+  pushBook: '/pushBook',
+  mine: '/mine',
+  bind: '/mine/bind',
+  bindPush: '/mine/push',
 };
 
 export const url = new Proxy(urlConfig, {
@@ -40,128 +38,111 @@ export const url = new Proxy(urlConfig, {
   }
 });
 
-const loginGet = (code, type = 'p', value = 0) => wxHelper.request(urlLogin, 'post', {
-  body: {
-    version: 1.0,
-    code: code,
-    type: type,
-    value: value,
-    app: 'NSJ'
-  }
-});
+const loginGet = (code) => wxHelper.request(url.login, 'get');
 
-const getMine = () => wxHelper.request(url.mine, 'get');
 
-const putMine = (param) => wxHelper.request(url.mine, 'put', {
-  body: param
-});
+// const loginGet = (code, type = 'p', value = 0) => wxHelper.request(urlLogin, 'post', {
+//   body: {
+//     version: 1.0,
+//     code: code,
+//     type: type,
+//     value: value,
+//     app: 'NSJ'
+//   }
+// });
 
-const customIndex = (menu, sub) => wxHelper.request(url.index, 'get', {
-  head: {
-    menu,
-    sub
-  }
-});
+// const getMine = () => wxHelper.request(url.mine, 'get');
 
-const customDetail = (id) => wxHelper.request(url.index, 'get', {
-  query: id
-});
+// const putMine = (param) => wxHelper.request(url.mine, 'put', {
+//   body: param
+// });
 
-const getPromote = () => wxHelper.request(url.promote, 'get');
+// const customIndex = (menu, sub) => wxHelper.request(url.index, 'get', {
+//   head: {
+//     menu,
+//     sub
+//   }
+// });
 
-const getCoupon = () => wxHelper.request(url.coupon, 'get');
+// const customDetail = (id) => wxHelper.request(url.index, 'get', {
+//   query: id
+// });
 
-const postCoupon = (id) => wxHelper.request(url.coupon, 'post', {
-  body: id
-});
+// const getPromote = () => wxHelper.request(url.promote, 'get');
 
-const getProduct = (id) => wxHelper.request(url.product, 'get', {
-  query: id
-});
+// const getCoupon = () => wxHelper.request(url.coupon, 'get');
 
-const getCart = () => wxHelper.request(url.cart, 'get');
+// const postCoupon = (id) => wxHelper.request(url.coupon, 'post', {
+//   body: id
+// });
 
-const postCart = (product, type, kind) => wxHelper.request(url.cart, 'post', {
-  body: {
-    product,
-    type,
-    kind
-  }
-});
+// const getProduct = (id) => wxHelper.request(url.product, 'get', {
+//   query: id
+// });
 
-const deleteCart = (id) => wxHelper.request(url.cart, 'delete', {
-  query: id
-});
+// const getCart = () => wxHelper.request(url.cart, 'get');
 
-const business = () => wxHelper.request(url.business, 'get');
+// const postCart = (product, type, kind) => wxHelper.request(url.cart, 'post', {
+//   body: {
+//     product,
+//     type,
+//     kind
+//   }
+// });
 
-const modifyPhone = (encryptedData, iv) => wxHelper.request(url.phone, 'put', {
-  body: {
-    data: encryptedData,
-    iv: iv
-  }
-});
+// const deleteCart = (id) => wxHelper.request(url.cart, 'delete', {
+//   query: id
+// });
 
-const getCode = (phone) => wxHelper.request(url.phone, 'get', {
-  head: {
-    phone
-  }
-});
+// const business = () => wxHelper.request(url.business, 'get');
 
-const modifyPhone2 = (id, code) => wxHelper.request(url.phone, 'put', {
-  body: {
-    id,
-    code
-  }
-});
+// const modifyPhone = (encryptedData, iv) => wxHelper.request(url.phone, 'put', {
+//   body: {
+//     data: encryptedData,
+//     iv: iv
+//   }
+// });
 
-const readOrder = (skip, take) => wxHelper.request(url.order, 'get', {
-  head: {
-    skip,
-    take
-  }
-});
+// const getCode = (phone) => wxHelper.request(url.phone, 'get', {
+//   head: {
+//     phone
+//   }
+// });
 
-const readOrderId = (id) => wxHelper.request(url.order, 'get', {
-  query: id
-});
+// const modifyPhone2 = (id, code) => wxHelper.request(url.phone, 'put', {
+//   body: {
+//     id,
+//     code
+//   }
+// });
 
-const ossGet = () => wxHelper.request(url.policy, 'get');
+// const readOrder = (skip, take) => wxHelper.request(url.order, 'get', {
+//   head: {
+//     skip,
+//     take
+//   }
+// });
 
-const getPay = (shop) => wxHelper.request(url.pay, 'get', {
-  head: {
-    shop: shop
-  }
-});
+// const readOrderId = (id) => wxHelper.request(url.order, 'get', {
+//   query: id
+// });
 
-const postPay = (mch, shop, price) => wxHelper.request(url.pay, 'post', {
-  body: {
-    mch: mch,
-    shop: shop,
-    price: price
-  }
-});
+// const ossGet = () => wxHelper.request(url.policy, 'get');
+
+// const getPay = (shop) => wxHelper.request(url.pay, 'get', {
+//   head: {
+//     shop: shop
+//   }
+// });
+
+// const postPay = (mch, shop, price) => wxHelper.request(url.pay, 'post', {
+//   body: {
+//     mch: mch,
+//     shop: shop,
+//     price: price
+//   }
+// });
 
 export default {
-  loginGet,
-  getMine,
-  putMine,
-  customIndex,
-  customDetail,
-  getPromote,
-  getCoupon,
-  postCoupon,
-  getProduct,
-  getCart,
-  postCart,
-  deleteCart,
-  business,
-  modifyPhone,
-  modifyPhone2,
-  getCode,
-  readOrder,
-  readOrderId,
-  ossGet,
-  getPay,
-  postPay
+  loginGet
 };
